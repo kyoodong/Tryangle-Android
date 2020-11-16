8a9fc22c9c96842ecbfcea1f645bdf0d276b27bf
이동규 <wind.dong.dream@gmail.com>
2020-11-16 10:55:39 +0900

    2주에 한 번씩 토큰 확인하는 작업

--- app/src/main/java/com/gomson/tryangle/SplashActivity.kt
+++ app/src/main/java/com/gomson/tryangle/SplashActivity.kt
@@ -17,8 +17,11 @@ import retrofit2.Response
 
 private const val MODEL_VERSION = "MODEL_VERSION"
 private const val FEATURE_VERSION = "FEATURE_VERSION"
+private const val LAST_VERSION_CHECKED = "LAST_VERSION_CHECKED"
 
 private const val TAG = "SplashActivity"
+
+private const val TOKEN_INTERVAL = 1000 * 60 * 60 * 24 * 14
 
 class SplashActivity : AppCompatActivity() {
 
@@ -52,7 +55,25 @@ class SplashActivity : AppCompatActivity() {
         editor.commit()
     }
 
+    private fun getLastVersionCheck(): Long {
+        val sharedPreferences =
+            baseContext.getSharedPreferences(baseContext.getString(R.string.app_name), Context.MODE_PRIVATE)
+        return sharedPreferences.getLong(LAST_VERSION_CHECKED, 0)
+    }
+
+    private fun setLastVersionCheck(time: Long) {
+        val sharedPreferences =
+            baseContext.getSharedPreferences(baseContext.getString(R.string.app_name), Context.MODE_PRIVATE)
+        val editor = sharedPreferences.edit()
+        editor.putLong(LAST_VERSION_CHECKED, time)
+        editor.commit()
+    }
+
     private fun goToMain() {
+        if (System.currentTimeMillis() - getLastVersionCheck() >= TOKEN_INTERVAL) {
+            setLastVersionCheck(System.currentTimeMillis())
+        }
+
         startActivity(Intent(this, MainActivity::class.java))
         finish()
     }
@@ -69,7 +90,6 @@ class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
-
     }
 
     override fun onResume() {
@@ -80,124 +100,134 @@ class SplashActivity : AppCompatActivity() {
         val modelVersion = getModelVersion()
         val featureVersion = getFeatureVersion()
 
-        showWait("토큰 발급 중...")
         Log.i(TAG, "토큰발급 요청")
-        modelService.issueToken(object : Callback<AccessToken> {
-            override fun onResponse(call: Call<AccessToken>, response: Response<AccessToken>) {
-                if (response.isSuccessful) {
-                    Log.i(TAG, "토큰발급 성공")
-                    var count = 0
-                    showWait("모델 버전 확인 중...")
-                    Log.i(TAG, "모델 버전 확인 요청")
-                    modelService.getLatestModelVersion(object: Callback<ResponseBody> {
-                        override fun onResponse(call: Call<ResponseBody>, response: Response<ResponseBody>) {
-                            if (response.isSuccessful) {
-                                Log.i(TAG, "모델 버전 로딩 성공")
-                                // 모델 버전이 다름
-                                val latestVersion = response.body()!!.string()
-                                if (modelVersion == null || modelVersion != latestVersion) {
-                                    Log.i(TAG, "모델 버전 다름")
-                                    showWait("최신버전 모델 다운로드 중...")
-                                    modelService.downloadModel(latestVersion, object: ModelService.ModelCallback {
-                                        override fun onSuccess() {
-                                            Log.i(TAG, "모델 다운로드 성공")
-                                            count++
 
-                                            if (count == 2) {
-                                                goToMain()
+        val lastVersionCheckTime = getLastVersionCheck()
+
+        // @TODO invalid token 체크
+
+        // 2주
+        if (System.currentTimeMillis() - lastVersionCheckTime >= TOKEN_INTERVAL) {
+            showWait("토큰 발급 중...")
+            modelService.issueToken(object : Callback<AccessToken> {
+                override fun onResponse(call: Call<AccessToken>, response: Response<AccessToken>) {
+                    if (response.isSuccessful) {
+                        Log.i(TAG, "토큰발급 성공")
+                        var count = 0
+                        showWait("모델 버전 확인 중...")
+                        Log.i(TAG, "모델 버전 확인 요청")
+                        modelService.getLatestModelVersion(object: Callback<ResponseBody> {
+                            override fun onResponse(call: Call<ResponseBody>, response: Response<ResponseBody>) {
+                                if (response.isSuccessful) {
+                                    Log.i(TAG, "모델 버전 로딩 성공")
+                                    // 모델 버전이 다름
+                                    val latestVersion = response.body()!!.string()
+                                    if (modelVersion == null || modelVersion != latestVersion) {
+                                        Log.i(TAG, "모델 버전 다름")
+                                        showWait("최신버전 모델 다운로드 중...")
+                                        modelService.downloadModel(latestVersion, object: ModelService.ModelCallback {
+                                            override fun onSuccess() {
+                                                Log.i(TAG, "모델 다운로드 성공")
+                                                count++
+
+                                                if (count == 2) {
+                                                    goToMain()
+                                                }
+                                                setModelVersion(latestVersion)
                                             }
-                                            setModelVersion(latestVersion)
-                                        }
 
-                                        override fun onFailure() {
-                                            Log.i(TAG, "모델 다운로드 실패")
-                                            Toast.makeText(baseContext, "모델 다운로드 실패", Toast.LENGTH_SHORT).show()
-                                            finish()
-                                        }
-                                    })
+                                            override fun onFailure() {
+                                                Log.i(TAG, "모델 다운로드 실패")
+                                                Toast.makeText(baseContext, "모델 다운로드 실패", Toast.LENGTH_SHORT).show()
+                                                finish()
+                                            }
+                                        })
+                                    } else {
+                                        Log.i(TAG, "모델 버전 같음")
+                                        count++
+                                    }
+
+                                    if (count == 2) {
+                                        goToMain()
+                                    }
                                 } else {
-                                    Log.i(TAG, "모델 버전 같음")
-                                    count++
+                                    Log.i(TAG, "모델 버전 로딩 실패")
+                                    Toast.makeText(baseContext, "모델 최신 버전 로딩 실패! ${response.code()}", Toast.LENGTH_SHORT).show()
+                                    finish()
                                 }
+                            }
 
-                                if (count == 2) {
-                                    goToMain()
-                                }
-                            } else {
+                            override fun onFailure(call: Call<ResponseBody>, t: Throwable) {
                                 Log.i(TAG, "모델 버전 로딩 실패")
-                                Toast.makeText(baseContext, "모델 최신 버전 로딩 실패! ${response.code()}", Toast.LENGTH_SHORT).show()
+                                t.printStackTrace()
+                                Toast.makeText(baseContext, "모델 최신 버전 로딩 실패!", Toast.LENGTH_SHORT).show()
                                 finish()
                             }
-                        }
+                        })
 
-                        override fun onFailure(call: Call<ResponseBody>, t: Throwable) {
-                            Log.i(TAG, "모델 버전 로딩 실패")
-                            t.printStackTrace()
-                            Toast.makeText(baseContext, "모델 최신 버전 로딩 실패!", Toast.LENGTH_SHORT).show()
-                            finish()
-                        }
-                    })
+                        Log.i(TAG, "피쳐 버전 확인 요청")
+                        modelService.getLatestFeatureVersion(object: Callback<ResponseBody> {
+                            override fun onResponse(call: Call<ResponseBody>, response: Response<ResponseBody>) {
+                                if (response.isSuccessful) {
+                                    Log.i(TAG, "피쳐 버전 로딩 성공")
 
-                    Log.i(TAG, "피쳐 버전 확인 요청")
-                    modelService.getLatestFeatureVersion(object: Callback<ResponseBody> {
-                        override fun onResponse(call: Call<ResponseBody>, response: Response<ResponseBody>) {
-                            if (response.isSuccessful) {
-                                Log.i(TAG, "피쳐 버전 로딩 성공")
+                                    val latestVersion = response.body()!!.string()
+                                    if (featureVersion == null || featureVersion != latestVersion) {
+                                        Log.i(TAG, "피쳐 버전 다름")
+                                        showWait("최신버전 피쳐 다운로드 중...")
+                                        modelService.downloadFeature(latestVersion, object: ModelService.ModelCallback {
+                                            override fun onSuccess() {
+                                                Log.i(TAG, "피쳐 다운로드 성공")
+                                                count++
 
-                                val latestVersion = response.body()!!.string()
-                                if (featureVersion == null || featureVersion != latestVersion) {
-                                    Log.i(TAG, "피쳐 버전 다름")
-                                    showWait("최신버전 피쳐 다운로드 중...")
-                                    modelService.downloadFeature(latestVersion, object: ModelService.ModelCallback {
-                                        override fun onSuccess() {
-                                            Log.i(TAG, "피쳐 다운로드 성공")
-                                            count++
-
-                                            if (count == 2) {
-                                                goToMain()
+                                                if (count == 2) {
+                                                    goToMain()
+                                                }
+                                                setFeatureVersion(latestVersion)
                                             }
-                                            setFeatureVersion(latestVersion)
-                                        }
 
-                                        override fun onFailure() {
-                                            Log.i(TAG, "피쳐 다운로드 실패")
-                                            Toast.makeText(baseContext, "피쳐 다운로드 실패!", Toast.LENGTH_SHORT).show()
-                                            finish()
-                                        }
-                                    })
+                                            override fun onFailure() {
+                                                Log.i(TAG, "피쳐 다운로드 실패")
+                                                Toast.makeText(baseContext, "피쳐 다운로드 실패!", Toast.LENGTH_SHORT).show()
+                                                finish()
+                                            }
+                                        })
+                                    } else {
+                                        Log.i(TAG, "피쳐 버전 같음")
+                                        count++
+                                    }
+                                    if (count == 2) {
+                                        goToMain()
+                                    }
                                 } else {
-                                    Log.i(TAG, "피쳐 버전 같음")
-                                    count++
+                                    Log.i(TAG, "피쳐 버전 로딩 실패")
+                                    Toast.makeText(baseContext, "피쳐 최신 버전 로딩 실패! ${response.code()}", Toast.LENGTH_SHORT).show()
+                                    finish()
                                 }
-                                if (count == 2) {
-                                    goToMain()
-                                }
-                            } else {
+                            }
+
+                            override fun onFailure(call: Call<ResponseBody>, t: Throwable) {
                                 Log.i(TAG, "피쳐 버전 로딩 실패")
-                                Toast.makeText(baseContext, "피쳐 최신 버전 로딩 실패! ${response.code()}", Toast.LENGTH_SHORT).show()
+                                t.printStackTrace()
+                                Toast.makeText(baseContext, "피쳐 최신 버전 로딩 실패!", Toast.LENGTH_SHORT).show()
                                 finish()
                             }
-                        }
+                        })
+                    } else {
+                        Log.i(TAG, "토큰발급 실패")
+                        Toast.makeText(baseContext, "토큰 발급 실패!", Toast.LENGTH_SHORT).show()
+                        finish()
+                    }
+                }
 
-                        override fun onFailure(call: Call<ResponseBody>, t: Throwable) {
-                            Log.i(TAG, "피쳐 버전 로딩 실패")
-                            t.printStackTrace()
-                            Toast.makeText(baseContext, "피쳐 최신 버전 로딩 실패!", Toast.LENGTH_SHORT).show()
-                            finish()
-                        }
-                    })
-                } else {
+                override fun onFailure(call: Call<AccessToken>, t: Throwable) {
                     Log.i(TAG, "토큰발급 실패")
                     Toast.makeText(baseContext, "토큰 발급 실패!", Toast.LENGTH_SHORT).show()
                     finish()
                 }
-            }
-
-            override fun onFailure(call: Call<AccessToken>, t: Throwable) {
-                Log.i(TAG, "토큰발급 실패")
-                Toast.makeText(baseContext, "토큰 발급 실패!", Toast.LENGTH_SHORT).show()
-                finish()
-            }
-        })
+            })
+        } else {
+            goToMain()
+        }
     }
 }
